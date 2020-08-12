8804790cd825f23b277e4255a8c26252d8caea18
Fadith Escorcia <feescorciam@protonmail.com>
2020-08-12 00:20:32 -0500

    Configuracion de Babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,4 +5,16 @@ module.exports = {
     filename: "app.bundle.js",
   },
   plugins: [new HtmlWebpackPlugin({ template: "src/index.html" })],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-env", "@babel/preset-react"] },
+        },
+      },
+    ],
+  },
 };
