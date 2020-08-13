ef6bdb10373efc63a98bdbecbf2402fc43c7beac
Fadith Escorcia <feescorciam@protonmail.com>
2020-08-12 22:32:56 -0500

    List of photocads component created

---
+++ src/components/ListOfPhotoCards/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { PhotoCard } from '../PhotoCard'
+
+export const ListOfPhotoCards = () =>
+  <ul>
+    {
+      [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7
+      ].map((id) => <PhotoCard key={id} />)
+    }
+  </ul>
