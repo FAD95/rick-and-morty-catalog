c8c76c2c6a73958c68b49a2f93e72a57cdb34751
Fadith Escorcia <feescorciam@protonmail.com>
2020-08-13 02:37:54 -0500

    Componente de categorias flotante creado

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -6,12 +6,23 @@ import Axios from 'axios'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState(mockCategories)
+  const [showFixed, setShowFixed] = useState(false)
   useEffect(() => {
     Axios.get('https://rick-and-morty-library-api.vercel.app/categories').then(res => setCategories(res.data))
   }, [])
+  useEffect(()=>{
+    const onScroll =()=>{
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return()=>document.removeEventListener('scroll', onScroll)
+  },[showFixed])
 
   const renderList = (fixed) => (
-    <List className={fixed?'fixed':''}>
+    <List fixed={fixed}>
       {
         categories.map(category => (
           <Item key={category.id}>
@@ -25,7 +36,7 @@ export const ListOfCategories = () => {
   return (
     <>
       {renderList()}
-      {renderList(true)}
+      {showFixed?renderList(showFixed):null}
     </>
   )
 }
