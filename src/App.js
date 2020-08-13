7713978d6661dc277739a6eea18bf09778399e7d
Fadith Escorcia <feescorciam@protonmail.com>
2020-08-12 22:32:03 -0500

    Logo agregado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { GlobalStyles } from './GloabalStyles'
+import { Logo } from './components/Logo'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 
 export const App = () =>
   <>
     <GlobalStyles />
+    <Logo/>
     <ListOfCategories />
     <ListOfPhotoCards />
   </>
