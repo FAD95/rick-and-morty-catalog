56b9b9fd297b6892aa7f7e8e4db2dc8f8bb7cc2b
Fadith Escorcia <feescorciam@protonmail.com>
2020-08-12 22:33:10 -0500

    Styles for Logo

---
+++ src/components/Logo/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const SvgContainer = styled.div`    
+    display: flex;
+    justify-content: center;    
+`
+
+export const Svg = styled.svg`
+    width:220px;
+    height: 80px;
+    display: flex;
+    justify-self: center;
+`
+
+export const Image = styled.image`
+    width:100%;    
+`
