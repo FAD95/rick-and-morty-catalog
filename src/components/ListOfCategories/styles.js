3eea55a502f41007c2886070793609d1440d6c83
Fadith Escorcia <feescorciam@protonmail.com>
2020-08-12 19:37:26 -0500

    Scroll bars removed

--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -4,9 +4,12 @@ export const List = styled.ul`
 display: flex;
 overflow:scroll;
 width: 100%;
-
+&::-webkit-scrollbar{
+    display: none;
+}
 `
 
 export const Item = styled.li`
 padding: 0 8px;
+width: 100%;
 `
