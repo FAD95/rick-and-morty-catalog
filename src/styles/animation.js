b8365d727684278ff21c202f6b292ad48047794e
Fadith Escorcia <feescorciam@protonmail.com>
2020-08-13 03:13:56 -0500

    From right animation created

--- src/styles/animation.js
+++ src/styles/animation.js
@@ -10,5 +10,21 @@ const fadeInKeyframes = keyframes`
         opacity:1;
     }
 `
+
+const fromRightKeyframes = keyframes`
+    from {
+        transform: translateX(200px) scale(.1) skew(30deg, 20deg);
+        filter:blur(5px);
+        opacity:0;
+    }
+    to {
+        transform: translateX(0) scale(.5) skew(0, 0);
+        filter:blur(0);
+        opacity:1;        
+    }
+`
 export const fadeIn = ({ time = '1s', type = 'ease' } = {}) =>
   css`animation: ${time} ${fadeInKeyframes} ${type};`
+
+  export const fromRight = ({ time = '1s', type = 'ease' } = {}) =>
+  css`animation: ${time} ${fromRightKeyframes} ${type};`
