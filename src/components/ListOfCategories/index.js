328a1d49d0e0c4fada954113285ba8e279daba55
Fadith Escorcia <feescorciam@protonmail.com>
2020-08-13 01:08:25 -0500

    Llamada a API

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
+import { categories as mockCategories } from '../../../api/db.json'
+import Axios from 'axios'
 
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const [categories, setCategories] = useState(mockCategories)
+  useEffect(() => {
+    Axios.get('https://rick-and-morty-library-api.vercel.app/categories').then(res => setCategories(res.data))
+  }, [])
+
+  const renderList = (fixed) => (
+    <List className={fixed?'fixed':''}>
       {
         categories.map(category => (
           <Item key={category.id}>
@@ -14,5 +20,12 @@ export const ListOfCategories = () => {
         ))
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {renderList(true)}
+    </>
   )
 }
