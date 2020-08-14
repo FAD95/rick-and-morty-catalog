24f3d2c79ccfa7e8d3fae3f6ae857595ff3459f8
Fadith Escorcia <feescorciam@protonmail.com>
2020-08-13 22:56:17 -0500

    Boton de like con cambio de color

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { ImgWrapper, Img, Button, Article } from './styles'
 import { AiFillExperiment } from 'react-icons/ai'
+import { IconContext } from 'react-icons/lib'
 
 const DEFAULT_IMG = 'https://www.tonica.la/__export/1593033191339/sites/debate/img/2020/06/24/rick-and-morty-portada.jpg_423682103.jpg'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMG }) => {
   const ref = useRef(null)
   const [show, setShow] = useState(false)
+  const [like, setLike] = useState(false)
 
   useEffect(() => {
     Promise.resolve(
@@ -22,6 +24,14 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMG }) => {
       observer.observe(ref.current)
     })
   }, [ref])
+
+  const Icon = ({ size }) => {
+    if (like) {
+      return <IconContext.Provider value={{ color: '#11bd32' }}><AiFillExperiment size={size} /></IconContext.Provider>
+    }
+    return <AiFillExperiment size={size} />
+  }
+
   return (
     <Article ref={ref}>
       {
@@ -32,13 +42,12 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMG }) => {
                 <Img src={src} alt='' />
               </ImgWrapper>
             </a>
-            <Button>
-              <AiFillExperiment size='32px' />
+            <Button onClick={() => setLike(!like)}>
+              <Icon size='32px' />
               {likes} likes!
             </Button>
           </>
       }
-
     </Article>
   )
 }
